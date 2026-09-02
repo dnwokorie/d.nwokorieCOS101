f40380c8f1cfeb7914b6bc4e7ed5dd1cbb679d7c
dnwokorie <dike.vincent-nwokorie@pau.edu.ng>
2026-09-02 16:06:56 +0100

    (My second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main(){
+	println!(); // prints just a
+
+	println!("hello ");//prints
+     
+    println!("format {} arguments", "some"); //prints format some arguments
+
+    println!("My name is {}. I am the CEO of Warrior Group", "Nwokorie Dike");
+
+}s
---
+++ week-2/practice_3.rs
@@ -0,0 +1,5 @@
+fn main() {
+	let fees = 2_000_000.00;
+	let salary: f64 = 3_000_000.00;
+	println!("fees is {} and salary is {}",fees,salary);
+}
