ea1b1a8094ca965a63672ffc1210ec1d5021a465
dnwokorie <dike.vincent-nwokorie@pau.edu.ng>
2026-09-02 17:27:42 +0100

    (My second Project)

---
+++ week-2/Projects/Project_2.rs
@@ -0,0 +1,11 @@
+fn main() {
+	// FORMULA TO CALCULATE THE SUM OF ALL SALES MADE
+	let a = (2.00 * 450_000.00) + (1_500_000.00) + (3.00 * 750_000.00) + (3.00 * 2_850_000.00) + (250_000.00);
+	println!("SUM is {}", a);  
+
+    
+    //FORMULA TO CALCULATE THE AVERAGE OF ALL SALES
+    let b = a / 10.0;
+    println!("AVERAGE is {}", b);
+    
+}
